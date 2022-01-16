1ddcf509c9dc7f2ef5f1922428ed5aadf2e34ab5
SubinJang <jsb408@gmail.com>
2022-01-16 12:35:50 +0900

    chore: 빌드 및 라이브러리 최신화 chore: ViewBinding 적용
    2022-01-16 12:35

--- app/src/main/java/com/goldouble/android/gtomlconverter/MainActivity.kt
+++ app/src/main/java/com/goldouble/android/gtomlconverter/MainActivity.kt
@@ -9,11 +9,11 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.goldouble.android.gtomlconverter.databinding.ActivityMainBinding
 import com.goldouble.android.gtomlconverter.db.Recipe
 import com.goldouble.android.gtomlconverter.db.RecipeDB
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
@@ -21,22 +21,25 @@ class MainActivity : AppCompatActivity() {
     private var recipeList = listOf<Recipe>()
     lateinit var mAdapter: RecipeAdapter
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Thread {
             recipeList = recipeDB?.recipeDao()?.getAll()!!
             mAdapter = RecipeAdapter(recipeList)
 
-            recipeRecyclerView.adapter = mAdapter
-            recipeRecyclerView.layoutManager = LinearLayoutManager(this)
+            binding.recipeRecyclerView.adapter = mAdapter
+            binding.recipeRecyclerView.layoutManager = LinearLayoutManager(this)
         }.start()
 
         MobileAds.initialize(this)
         //MobileAds.initialize(this, getString(R.string.admob_app_id))
         val adRequest = AdRequest.Builder().build()
-        adView.loadAd(adRequest)
+        binding.adView.loadAd(adRequest)
 
         val unitData: HashMap<String, Int> = hashMapOf()
         val itemList = resources.getStringArray(R.array.units)
@@ -45,68 +48,68 @@ class MainActivity : AppCompatActivity() {
             unitData[itemList[i]] = unitList[i]
 
         itemList.sort()
-        itemSpinner.adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, itemList)
+        binding.itemSpinner.adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, itemList)
 
-        itemSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        binding.itemSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 try {
-                    if (itemSpinner.getItemAtPosition(position).toString().substring(0, 2) == "계란" ||
-                        itemSpinner.getItemAtPosition(position).toString().substring(0, 3) == "egg")
-                        eggAlert.visibility = View.VISIBLE
-                    else eggAlert.visibility = View.GONE
+                    if (binding.itemSpinner.getItemAtPosition(position).toString().substring(0, 2) == "계란" ||
+                        binding.itemSpinner.getItemAtPosition(position).toString().substring(0, 3) == "egg")
+                        binding.eggAlert.visibility = View.VISIBLE
+                    else binding.eggAlert.visibility = View.GONE
                 } catch(e: Exception) {
                     Log.d("MainActivity", e.toString())
-                    eggAlert.visibility = View.GONE
+                    binding.eggAlert.visibility = View.GONE
                 }
-                convertedText.setText("0")
+                binding.convertedText.setText("0")
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) = Unit
         }
 
-        convertedText.addTextChangedListener {
-            if (convertedText.isFocused) {
+        binding.convertedText.addTextChangedListener {
+            if (binding.convertedText.isFocused) {
                 try {
                     val converted = it.toString().toDouble()
-                    val gravity = unitData[itemSpinner.selectedItem.toString()]!!.toDouble() / 200.0
+                    val gravity = unitData[binding.itemSpinner.selectedItem.toString()]!!.toDouble() / 200.0
 
                     Log.d("converted", converted.toString())
                     Log.d("gravity", gravity.toString())
                     Log.d("mL", (converted / gravity).toString())
-                    convertText.setText((converted / gravity).roundToInt().toString())
+                    binding.convertText.setText((converted / gravity).roundToInt().toString())
                 } catch (e: Exception) {
                     Log.e("MainActivity", e.toString())
-                    if (it!!.isEmpty()) convertText.setText("")
+                    if (it!!.isEmpty()) binding.convertText.setText("")
                 }
             }
         }
 
-        convertText.addTextChangedListener {
-            if (convertText.isFocused) {
+        binding.convertText.addTextChangedListener {
+            if (binding.convertText.isFocused) {
                 try {
                     val convert = it.toString().toDouble()
-                    val gravity = unitData[itemSpinner.selectedItem.toString()]!!.toDouble() / 200.0
+                    val gravity = unitData[binding.itemSpinner.selectedItem.toString()]!!.toDouble() / 200.0
 
                     Log.d("convert", convert.toString())
                     Log.d("gravity", gravity.toString())
                     Log.d("g", (convert * gravity).toString())
-                    convertedText.setText((convert * gravity).roundToInt().toString())
+                    binding.convertedText.setText((convert * gravity).roundToInt().toString())
                 } catch (e: Exception) {
                     Log.e("MainActivity", e.toString())
-                    if (it!!.isEmpty()) convertedText.setText("")
+                    if (it!!.isEmpty()) binding.convertedText.setText("")
                 }
             }
         }
 
-        addBtn.setOnClickListener {
+        binding.addBtn.setOnClickListener {
             try {
-                val converted = convertedText.text.toString().toInt()
-                val convert = convertText.text.toString().toInt()
+                val converted = binding.convertedText.text.toString().toInt()
+                val convert = binding.convertText.text.toString().toInt()
 
                 val addRunnable = Runnable {
                     val newRecipe = Recipe(
                         null,
-                        itemSpinner.selectedItem.toString(),
+                        binding.itemSpinner.selectedItem.toString(),
                         converted,
                         convert)
                     recipeDB?.recipeDao()?.insert(newRecipe)
@@ -116,7 +119,7 @@ class MainActivity : AppCompatActivity() {
                 val addThread = Thread(addRunnable)
                 addThread.start()
 
-                Toast.makeText(this, itemSpinner.selectedItem.toString() + getString(R.string.add_msg), Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, binding.itemSpinner.selectedItem.toString() + getString(R.string.add_msg), Toast.LENGTH_SHORT).show()
                 Log.d("LIST", recipeList.toString())
                 mAdapter.notifyDataSetChanged()
             } catch (e: Exception) {
